ce584332116ddf11d8d424e7ae60ba233533c81c
Suraj Kumar Jana <surajjana2@gmail.com>
2016-02-07 00:29:55 +0530

    few changes

--- add.py
+++ add.py
@@ -12,7 +12,7 @@ while(True):
 
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
